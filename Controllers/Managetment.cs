bde0a479aa90de83fdb0627ed5dd85c5721e11af
tuanct413 <tuanct413@gmail.com>
2025-03-18 14:49:02 +0700

    them manager User

---
+++ Controllers/Managetment.cs
@@ -0,0 +1,39 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using static Demo.Models.User;
+
+namespace Demo.Controllers
+{
+    public class Managetment : Controller
+    {
+
+        private static UserList _userList = new UserList(); // Tạo danh sách User
+
+        public IActionResult Index()
+        {
+            var users = _userList.Users; // Lấy danh sách user từ UserList
+            return View(users); // Truyền danh sách user ra View
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (_userList.Users.Any(u => u.Email == user.Email))
+            {
+                ModelState.AddModelError("Email", "❌ Email đã tồn tại! Vui lòng nhập email khác.");
+                return View(user); // Trả lại view kèm theo lỗi
+            }
+
+            _userList.Users.Add(user);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
