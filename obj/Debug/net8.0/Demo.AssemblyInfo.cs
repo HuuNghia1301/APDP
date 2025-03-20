4a8effcd28bfc158149d435a8b49f637088a323f
Trung Ton <trungton883@gmail.com>
2025-03-20 21:49:30 +0700

    update

--- obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45bf8e192aeff15a7e3e723777c452401e4ad5da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d987e0af7f3b21b1e0976a6acb9d72cd7a87e5f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
